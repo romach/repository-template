8e18d50ea43ab7a49c04766c8cd15b3f277b7166
romach <romach007@gmail.com>
2017-05-14 21:21:28 +0300

    Add repository creation script

---
+++ create-repository-from-template.sh
@@ -0,0 +1,8 @@
+USER_NAME=$1
+REPOSITORY_NAME=$2
+
+git clone git@github.com:romach/repository-template.git .
+git remote rm origin
+curl -u $USER_NAME https://api.github.com/user/repos -d '{"name":"$REPOSITORY_NAME"}'
+git remote add origin git@github.com:$USER_NAME/$REPOSITORY_NAME.git
+git push origin master
